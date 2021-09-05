2b5b51e7d74c319fa3c7357bd5125ed59e93550b
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-09-05 17:45:10 +0700

    finish fetching movies and ui

--- browser-side/src/components/MovieList/index.jsx
+++ browser-side/src/components/MovieList/index.jsx
@@ -1,91 +1,57 @@
 import React from 'react';
-
-const MovieList = () => {
+import { NavLink } from 'react-router-dom';
+import './movielist.scss'
+const MovieList = ({ arrMovies }) => {
 
     return (
-        <section className="text-gray-600 body-font">
+        <section>
             <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-wrap -m-4">
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
-                        </a>
-                        <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                            <p className="mt-1">$16.00</p>
-                        </div>
-                    </div>
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261" />
-                        </a>
-                        <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                            <p className="mt-1">$21.15</p>
-                        </div>
-                    </div>
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262" />
-                        </a>
-                        <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                            <p className="mt-1">$12.00</p>
-                        </div>
-                    </div>
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263" />
-                        </a>
-                        <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                            <p className="mt-1">$18.40</p>
-                        </div>
-                    </div>
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264" />
-                        </a>
-                        <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                            <p className="mt-1">$16.00</p>
-                        </div>
-                    </div>
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265" />
-                        </a>
-                        <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                            <p className="mt-1">$21.15</p>
-                        </div>
-                    </div>
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267" />
-                        </a>
-                        <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                            <p className="mt-1">$12.00</p>
-                        </div>
-                    </div>
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268" />
-                        </a>
-                        <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                            <p className="mt-1">$18.40</p>
-                        </div>
-                    </div>
+
+                <div className="grid grid-cols-4 gap-8">
+                    {arrMovies?.map((movie, index) => {
+                        const { trailer, tenPhim, moTa, hinhAnh, danhGia } = movie
+                        return (
+                            <div className="cards px-4 w-full rounded-lg overflow-hidden" key={index}>
+                                <NavLink to="/" className="card-thumbnail block relative h-96 rounded overflow-hidden">
+                                    <img
+                                        className="thumbnail-img object-cover object-center w-full h-full "
+                                        src={hinhAnh}
+                                        onError={e => (e.target.src = "https://picsum.photos/264/370/")}
+                                        alt="movie" />
+                                </NavLink>
+                                <div className="px-5 bg-bgColorDetail" >
+                                    <h5 className="py-6 uppercase text-2xl border-b-2 border-dashed border-yellow-500 text-white font-semibold text-left
+                                    align-middle
+                                    "
+                                        style={{ minHeight: "120px" }}
+                                    >
+                                        {tenPhim}
+                                    </h5>
+                                    <div className="py-1 flex align-middle justify-start">
+                                        <div className="flex align-middle w-auto pr-5">
+                                            <div className="align-baseline">
+                                                <img className="object-cover w-full object-center leading-7 mt-1"
+                                                    src="http://pixner.net/boleto/demo/assets/images/movie/tomato.png"
+
+                                                    alt="tomato" />
+                                            </div>
+                                            <span className="pl-2 text-white font-bold text-lg self-center leading-7">{danhGia * 10}%</span>
+                                        </div>
+                                        <div className="flex align-middle w-auto pr-5">
+                                            <div className="align-baseline">
+                                                <img className="object-cover w-full object-center leading-7 mt-1"
+                                                    src="http://pixner.net/boleto/demo/assets/images/movie/cake.png" alt="tomato" />
+                                            </div>
+                                            <span className="pl-2 text-white font-bold text-lg self-center leading-7">{danhGia * 10}%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        )
+                    })}
+
+
                 </div>
             </div>
         </section>
--- browser-side/src/pages/Home/index.jsx
+++ browser-side/src/pages/Home/index.jsx
@@ -1,24 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import HomeCarousel from '../../components/HomeCarousel';
 import { useDispatch, useSelector } from 'react-redux';
 import { getArrMoviesPagination } from '../../store/actions/ManageMovieAction';
 import MovieList from '../../components/MovieList';
+import { Pagination } from 'antd';
+import { DoubleLeftOutlined, DoubleRightOutlined } from '@ant-design/icons';
+import './home.scss'
 
 const HomePage = () => {
 
     const dispatch = useDispatch();
     const arrMoviesPagination = useSelector(state => state.MovieReducer.arrMoviesPagination);
+    console.log(arrMoviesPagination)
+    const { items, totalCount } = arrMoviesPagination || {}
 
     const [page, setPage] = useState(1);
+    const onChange = (page) => {
+        setPage(page)
+        console.log(page)
+    }
+    function itemRender(current, type, originalElement) {
+        if (type === 'prev') {
+            return <a>
+                <DoubleLeftOutlined style={{ color: "#5560ff", position: 'relative', transform: "translateY(-4px)", marginRight: "5px" }} />
+                <span className="text-base text-indigo-500">Prev</span>
+            </a>;
+        }
+        if (type === 'next') {
+            return <a>
+                <span className="text-base text-indigo-500">Next</span>
+                <DoubleRightOutlined style={{ color: "#5560ff", position: 'relative', transform: "translateY(-4px)", marginLeft: "5px" }} />
+            </a>
+        }
 
-
-    const fetchArrMoviesPagination = () => {
-        dispatch(getArrMoviesPagination())
+        return originalElement;
     }
 
-    useEffect(() => {
 
-    }, [])
+    const fetchArrMoviesPagination = useCallback((page) => {
+        dispatch(getArrMoviesPagination(page))
+    }, [dispatch])
+
+    useEffect(() => {
+        fetchArrMoviesPagination(page)
+
+    }, [fetchArrMoviesPagination, page])
 
     return (
         <div>
@@ -39,8 +65,22 @@ const HomePage = () => {
                         </div>
                     </div>
                     <div>
-                        <MovieList />
-
+                        <MovieList arrMovies={items} />
+                        <Pagination
+                            onChange={onChange}
+                            current={page}
+                            defaultCurrent={1}
+                            total={totalCount}
+                            itemRender={itemRender}
+                            showSizeChanger={false}
+                            showTotal={total => {
+                                return (
+                                    <span className="text-white text-sm">
+                                        {`Total ${total} movies`}
+                                    </span>
+                                )
+                            }}
+                        />
                     </div>
                 </div>
             </section>
@@ -48,4 +88,4 @@ const HomePage = () => {
     );
 };
 
-export default HomePage;
+export default memo(HomePage);
