da2f0a1c349bef48dc7e94a602c00b8931c8c18a
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-09-12 16:08:29 +0700

    set up checked in payment button

--- browser-side/src/pages/Checkout/index.jsx
+++ browser-side/src/pages/Checkout/index.jsx
@@ -1,15 +1,15 @@
-import { CloseOutlined, LoadingOutlined, SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
-import { Steps } from 'antd';
-import React, { Fragment, useCallback, useEffect, useState } from 'react';
+import { CloseOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Steps, Tabs } from 'antd';
+import _ from 'lodash';
+import React, { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
+import BookingTicketInfo from '../../core/models/bookingTicketInfo';
 import createAction from '../../store/actions/createAction';
 import { getBookingInfo, getBookingTicketInfo } from '../../store/actions/MangeBookingAction';
 import { actionTypes } from '../../store/actions/Types';
-import _ from 'lodash';
+import formMoney from '../../utils/formMoney';
 import './checkout.scss';
-import BookingTicketInfo from '../../core/models/bookingTicketInfo';
-import { Tabs } from 'antd';
 
 const { TabPane } = Tabs;
 
@@ -19,10 +19,17 @@ const CheckoutPage = () => {
     const currentUser = useSelector(state => state.UserReducer.currentUser);
     console.log(currentUser)
     const { bookingInfo, onBookingArr } = useSelector(state => state.BookingReducer);
-    console.log('booking', bookingInfo)
+
+    // const inputCheck = useRef();
+    // const activeButton = inputCheck.current.checked
+
+    const [checked, setChecked] = useState(false)
+
+    const handleChange = (e) => {
+        setChecked(e.target.checked)
+    }
 
     const movieId = useParams()
-    console.log(movieId.id)
 
 
     const dispatch = useDispatch();
@@ -35,38 +42,49 @@ const CheckoutPage = () => {
         fetchBooking()
     }, [fetchBooking])
 
+
+
+    const handleSendBookingTicket = () => {
+        if (!checked) return
+        const bookingTicketInfo = new BookingTicketInfo();
+        bookingTicketInfo.maLichChieu = movieId.id;
+        bookingTicketInfo.danhSachVe = onBookingArr;
+        dispatch(getBookingTicketInfo(bookingTicketInfo))
+    }
+
     const { thongTinPhim, danhSachGhe } = bookingInfo
 
     const renderSeats = () => {
         return danhSachGhe.map((seat, index) => {
 
-            let classGheVip = seat.loaiGhe === 'Vip' ? 'gheVip' : '';
-            let classGheDaDat = seat.daDat === true ? 'gheDaDat' : '';
-            let classGheDangDat = ''
-            let indexGheDangDat = onBookingArr.findIndex(onBooking => onBooking.maGhe === seat.maGhe);
-            if (indexGheDangDat !== -1) {
-                classGheDangDat = 'gheDangDat'
+            let classVipSeat = seat.loaiGhe === 'Vip' ? 'seatVip' : '';
+            let classSeatServed = seat.daDat === true ? 'seatServed' : '';
+            let classSeatOnChoose = ''
+            let indexSeatOnChoose = onBookingArr.findIndex(onBooking => onBooking.maGhe === seat.maGhe);
+            if (indexSeatOnChoose !== -1) {
+                classSeatOnChoose = 'seatOnChoose'
             }
-            let classGheDaDuocDat = '';
+            let classChooseByMe = '';
             if (currentUser?.taiKhoan === seat.taiKhoanNguoiDat) {
-                classGheDaDuocDat = 'gheDaDuocDat'
+                classChooseByMe = 'seatChooseByMe'
             }
 
             return (
                 <Fragment key={index}>
-                    {/* Cách css 1 */}
-                    {/* {seat.loaiGhe === 'Vip'
-
-                        ? <button className="ghe gheVip" key={index}>{seat.stt}</button>
-                        : <button className="ghe" key={index}>{seat.stt}</button>
-                    } */}
-                    {/* Cách css 2 */}
                     <button onClick={() => {
                         dispatch(createAction(actionTypes.BOOKING_SEAT, seat))
                     }}
-                        disabled={seat.daDat} className={`ghe ${classGheVip} ${classGheDaDat} ${classGheDangDat} ${classGheDaDuocDat}`}
+                        disabled={seat.daDat} className={`
+                        w-8 h-8 rounded-lg m-1 bg-blue-500 
+                        hover:bg-greenText transition duration-300 ease-in-out
+                        ${classVipSeat} 
+                        ${classSeatServed} 
+                        ${classSeatOnChoose} 
+                        ${classChooseByMe}`}
                         key={index}>
-                        {seat.daDat ? classGheDaDuocDat !== '' ? <UserOutlined /> : <CloseOutlined /> : seat.stt}
+                        {seat.daDat ? classChooseByMe !== '' ?
+                            <UserOutlined style={{ color: "#31d7a9" }} />
+                            : <CloseOutlined /> : seat.stt}
                     </button>
                     {(index + 1) % 16 === 0 ? <br /> : ''}
                 </Fragment>
@@ -76,33 +94,61 @@ const CheckoutPage = () => {
 
 
     return (
-        <div className="max-h-full">
+        <div className=" py-4">
             <div className="grid grid-cols-12 ">
-                <div className="col-span-9">
-                    <div className="p-5 border-b-4 border-black">
+                <div className="col-span-8">
+                    {/* <div className="p-5 border-b-4 border-black">
                         <Steps>
                             <Step status="finish" title="Verification" icon={<SolutionOutlined />} />
                             <Step status="process" title="Pay" icon={<LoadingOutlined />} />
                             <Step status="wait" title="Done" icon={<SmileOutlined />} />
                         </Steps>
-                    </div>
-                    <div className="flex items-center justify-center mt-5 flex-col">
+                    </div> */}
+                    {/* <div className="flex items-center justify-center mt-5 flex-col">
                         <div className="bg-black w-5/6">
                             screen
                         </div>
                         <div className="trapezoid"></div>
+                    </div> */}
+                    <div className="py-5">
+                        <div className="flex justify-center align-middle">
+                            <img src="http://pixner.net/boleto/demo/assets/images/movie/screen-thumb.png" alt="screen" />
+                        </div>
+                        <h4 className="text-xl text-white font-bold">Screen</h4>
                     </div>
+
+
                     <div>
                         {renderSeats()}
                     </div>
-                    <div className="mt-5 flex justify-center">
+                    <div className="mt-5 flex justify-evenly">
                         <table className="divide-y divide-gray-200 w-2/3 table-auto">
                             <thead>
-                                <tr>
-                                    <th>Ghế chưa đặt</th>
-                                    <th>Ghế đang đặt</th>
-                                    <th>Ghế vip</th>
-                                    <th>Ghế đã được đặt</th>
+                                <tr className="flex space-x-4">
+                                    <th className="flex justify-center">
+                                        <button className="w-8 h-8 rounded-lg m-1 bg-blue-500 "></button>
+                                        <span className="text-white leading-10">Available</span>
+                                    </th>
+                                    <th className="flex justify-center">
+                                        <button className="w-8 h-8 rounded-lg m-1 bg-yellow-500 "></button>
+                                        <span className="text-white leading-10">Vip</span>
+                                    </th>
+                                    <th className="flex justify-center">
+                                        <button className="w-8 h-8 rounded-lg m-1 bg-red-500 ">
+                                            <CloseOutlined />
+                                        </button>
+                                        <span className="text-white leading-10">Served</span>
+                                    </th>
+                                    <th className="flex justify-center">
+                                        <button className="w-8 h-8 rounded-lg m-1 bg-green-500 "></button>
+                                        <span className="text-white leading-10">Choosing</span>
+                                    </th>
+                                    <th className="flex justify-center">
+                                        <button className="w-8 h-8 rounded-lg m-1 bg-white text-greenText ">
+                                            <UserOutlined />
+                                        </button>
+                                        <span className="text-white leading-10">Choose by me</span>
+                                    </th>
                                 </tr>
                             </thead>
                         </table>
@@ -110,54 +156,70 @@ const CheckoutPage = () => {
 
 
                 </div>
-                <div className="col-span-3 text-left">
-                    <h3 className="text-green-500 text-2xl">0 vnd</h3>
+                <div className="p-5 col-span-4 divide-y-2 divide-yellow-600 divide-dashed">
+                    <h3 className="text-white text-3xl text-center pb-4">
+                        BOOKING SUMMARY
+                    </h3>
 
-                    <hr />
+                    <div className="py-2 text-left text-white">
+                        <h3 className="text-xl text-greenText">{thongTinPhim.tenPhim}</h3>
+                        <p>Address: <span className="text-gray-500">{thongTinPhim.tenCumRap} - {thongTinPhim.tenRap}</span></p>
+                        <p>Show Time: <span className="text-gray-500">{thongTinPhim.ngayChieu} - {thongTinPhim.gioChieu}</span> </p>
+                    </div>
 
-                    <h3 className="text-xl">{thongTinPhim.tenPhim}</h3>
-                    <p>Địa điểm: {thongTinPhim.tenCumRap} - {thongTinPhim.tenRap}</p>
-                    <p>Ngày chiếu: {thongTinPhim.ngayChieu} - {thongTinPhim.gioChieu}</p>
-
-                    <hr />
-
-                    <div className="flex flex-row my-5">
-                        <div className="w-4/5">
-                            <span className="text-red-400">Ghế</span>
+                    <div className="flex flex-row py-2">
+                        <div className="flex-none">
+                            <span className="text-white text-lg">Seats:</span>
+                        </div>
+                        <div className="flex-grow flex-wrap">
                             {_.sortBy(onBookingArr, ['stt']).map((onBookingSeat, index) => {
-                                return <span key={index} className="text-base text-green-500 m-1">
+                                return <span key={index} className="text-base text-gray-500 m-1">
                                     {onBookingSeat.stt}
                                 </span>
                             })}
                         </div>
+
                         <div className="text-right">
                             <span className="text-green-500 text-lg">
-                                {onBookingArr.reduce((total, seat, index) => {
-                                    return total += seat.giaVe
-                                }, 0).toLocaleString()}
+                                {formMoney(
+                                    onBookingArr.reduce((total, seat, index) => {
+                                        return total += seat.giaVe
+                                    }, 0)
+                                )}
                             </span>
                         </div>
                     </div>
 
-                    <hr />
-                    <div className="my-5">
-                        <p>Email</p>
-                        <span>{currentUser?.email}</span>
+                    <div className="py-2  text-left">
+                        <p className="text-white">User name: <span className="text-gray-500">{currentUser?.taiKhoan}</span></p>
+                        <p className="text-white">Email: <span className="text-gray-500">{currentUser?.email}</span></p>
+                        <p className="text-white">Phone: <span className="text-gray-500">{currentUser?.soDT}</span></p>
                     </div>
-                    <div className="my-5">
-                        <p>Phone</p>
-                        <span>{currentUser?.soDT}</span>
+
+                    <div className="py-2">
+                        <label className="inline-flex items-center">
+                            <input type="checkbox" className="form-checkbox h-14 w-14 text-indigo-600"
+                                onChange={(e) => handleChange(e)} />
+                            <p className="ml-2 text-white">
+                                I Accept" button, you represent that you have read and understand this agreement, any additional terms and conditions imposed by the promoter
+                            </p>
+                        </label>
                     </div>
-                    <hr />
-                    <div className="mb-5 fixed bottom-0 transform translate-x-2/4">
-                        <button className="button--action " onClick={() => {
-                            const bookingTicketInfo = new BookingTicketInfo();
-                            bookingTicketInfo.maLichChieu = movieId.id;
-                            bookingTicketInfo.danhSachVe = onBookingArr;
-                            dispatch(getBookingTicketInfo(bookingTicketInfo))
-                        }}>
+
+                    <div className="pt-5">
+                        <button
+                            className={
+                                `px-12 py-3 font-semibold text-sm rounded-full text-white duration-300 ease-in-out min-w-52
+                            ${checked ? `bg-gradient-to-r from-yellow-400 via-red-500 to-indigo-700 hover:shadow-button`
+                                    : `disabled:opacity-50 bg-transparent border-2 border-indigo-500 cursor-not-allowed`
+                                }
+                        `}
+                            onClick={() => handleSendBookingTicket()}
+                        >
+
                             BOOKING TICKET
                         </button>
+
                     </div>
 
                 </div>
@@ -277,7 +339,7 @@ const Demo = (props) => {
         <Tabs defaultActiveKey="1"
             className="customTab"
             tabBarStyle={{
-                color: '#fff', height: '10vh', fontSize: '18px',
+                color: '#fff', height: '10vh', fontSize: '17px',
                 fontWeight: 'bold',
                 backgroundColor: "#032055",
                 padding: "8px"
@@ -286,7 +348,7 @@ const Demo = (props) => {
             <TabPane tab="01 CHOOSE YOUR SEAT" key="1" className="">
                 <CheckoutPage {...props} />
             </TabPane>
-            <TabPane tab="02 HISTORY" key="2">
+            <TabPane tab="02 BOOKING HISTORY" key="2">
                 <CheckOutFinal {...props} />
             </TabPane>
         </Tabs>
